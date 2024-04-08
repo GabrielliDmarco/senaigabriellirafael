8032d6f16c814f4a3867c65ed947ec91221ecd43
GabrielliDmarco <163335450+GabrielliDmarco@users.noreply.github.com>
2024-04-08 17:17:08 -0300

    Exercícios de lógica

---
+++ exercício.js
@@ -0,0 +1,94 @@
+//1
+//let contador = 1
+//console.log("1")
+ 
+//while(contador <=100){
+   // console.log(contador)
+    //contador++
+//}
+
+ //2 e 3
+ // let tabuada, result =  ""
+//tabuada = Number(prompt("Digite um número"))
+//for (let i = 1; i <= 10; i++){
+    //result += tabuada + "X" + i + "="
+    //+ (tabuada * i) + "\n"
+//}
+//alert(result)
+
+//4
+//for(let i = 10; i >= 0; i--){
+    //console.log(i)
+//}
+
+//5
+//for(let i = 1; i<=20; i++){
+    //if(i % 3 == 0 && i % 2 !=0)
+    //console.log(i)
+//}
+
+//6
+//for(let i = 1; i<=10; i*=2){
+   // console.log(1)
+//}
+
+//7
+//for(let i=1; i<=10; i++ ){
+    //console.log(i)
+//}
+//for(let i=9; i>1; i--){
+    //console.log(i)
+//}
+
+//8
+//let fatorial = 1
+//for(let i= Number(prompt("Digite um número: ")); i >=1; i--){
+//    fatorial *=i
+//}
+//alert(fatorial)
+
+//9
+//for(let i=1; i<=100; i++){
+  //  if( i % 3 == 0 && 1 % 5 == 0){
+  //      console.log("FizzBuzz");
+  //  }else if (i % 3 == 0){
+   //     console.log("Fizz");
+   // }else if (i % 5 == 0){
+   ///     console.log("Buzz");
+   // }else{
+   //     console.log(i)
+   // }
+//}
+
+//10
+//prompt("Digite um número: ")
+//let divisao = 0
+//for(let i=1; i<=10; i++){
+//    divisao += 1 / i
+//}
+//alert(divisao)
+
+//11
+//function contarPareseImpares(){
+   // let pares = 0;
+  //  let impares = 0;
+ //   
+ //   for (let i = 1; i<=100; i++){
+  //      if(i % 2 === 0){
+ //           pares++;
+ //       }else{
+//        impares++;
+ //       }
+ //   }
+ //   console.log(`A quantidade de números pares é: ${pares}`);
+//    console.log(`A quantidade de número impares é: ${impares}`);
+//}
+//contarPareseImpares();
+  
+    
+
+
+
+
+
+
